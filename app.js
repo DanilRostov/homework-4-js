66430906357ea7923e84670134c8f70aba592eae
DanilRostov <daniil.rossy@gmail.com>
2018-06-23 16:57:39 +0300

    removed dispensable code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const largest = (...args) => Math.max.apply(Math, args.map(i => i));
-const smallest = (...args) => Math.min.apply(Math, args.map(i => i));
+const largest = (...args) => Math.max.apply(Math, args);
+const smallest = (...args) => Math.min.apply(Math, args);
 const transform = (arr) => arr.map(i => f => i);
 const sum = (...args) => args.length <= 0 ? 0 : args[args.length - 1] + sum(...args.slice(0, -1));
 const sumSmallVariant = (...args) => args.reduce((sum, current) => sum + current);
